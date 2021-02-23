b7208aa91e8da48bc3d3d848ffe83cfd150c99dd
yuelinghou <76507035+yuelinghou@users.noreply.github.com>
2021-02-24 00:01:51 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,144 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 //#include<stdio.h>
+//#include<math.h>
 //
 //int main()
 //{
-//	int sta=0;
-//	int end = 0;
-//	scanf("%d%d", &sta, &end);
-//	int sta_h = sta / 100;
-//	int sta_m = sta % 100;
-//	int end_h = end / 100;
-//	int end_m = end % 100;
-//	if (sta_m > end_m)
-//	{
-//		printf("%02d:%02d\n", end_h-sta_h -1, 60 - sta_m + end_m);
-//	}
-//	else
-//	{
-//		if (60 - sta_m + end_m >= 60)
-//		{
-//			printf("%02d:%02d\n", end_h - sta_h, end_m - sta_m);
-//		}
-//		else
-//		{
-//			printf("%02d:%02d\n", end_h - sta_h, 60 + end_m - sta_m);
-//		}
-//	}
-//	return 0;
-//}
-
-//#include<stdio.h>
-//
-//int main()
-//{
+//	int n = 0;
+//	scanf("%d", &n);
 //	int i = 0;
-//	int sum = 0;
-//	for (i = 1; i <= 100; i++)
+//	for (i = 0; i < n + 1; i++)
 //	{
-//		sum += i;
+//		printf("pow(3,%d) = %.0lf\n", i, pow(3, i));
 //	}
-//	printf("sum = %d\n", sum);
-//	return 0;
-//}
-
-//#include<stdio.h>
-//
-//int main()
-//{
-//	int N = 0;
-//	scanf("%d", &N);
-//	int i = 0;
-//	double ret = 0.0;
-//	for (i = 1; i <= N; i++)
-//	{
-//		ret += 1.0 / i;
-//	}
-//	printf("sum = %.6lf\n", ret);
-//	return 0;
-//}
-
-//#include<stdio.h>
-//
-//int main()
-//{
-//	int N = 0;
-//	scanf("%d", &N);
-//	int i = 0;
-//	int j = 1;
-//	double ret = 0.0;
-//	for (i = 0; i < N; i++)
-//	{
-//		ret += 1.0 / j;
-//		j += 2;
-//	}
-//	printf("sum = %.6lf\n", ret);
-//	return 0;
-//}
-
-//#include<stdio.h>
-//
-//int main()
-//{
-//	int N = 0;
-//	scanf("%d", &N);
-//	int i = 0;
-//	int j = 1;
-//	int k = 1;
-//	double ret = 0.0;
-//	for (i = 0; i < N; i++)
-//	{
-//		ret += k * 1.0 / j;
-//		j += 3;
-//		k *= -1;
-//	}
-//	printf("sum = %.3lf\n", ret);
-//	return 0;
-//}
-
-//#include<stdio.h>
-//
-//int main()
-//{
-//	int lower = 0;
-//	int upper = 0;
-//	scanf("%d%d", &lower, &upper);
-//	if (lower > upper || upper > 100)
-//	{
-//		printf("Invalid.\n");
-//	}
-//	else
-//	{
-//		printf("fahr celsius\n");
-//		int i = 0;
-//		for (i = lower; i < upper + 1; i += 2)
-//		{
-//			printf("%d%6.1lf\n", i, 5.0*(i - 32) / 9);
-//		}
-//	}
-//	return 0;
-//}
-
-//#include<stdio.h>
-//
-//int main()
-//{
-//	int N = 0;
-//	scanf("%d", &N);
-//	int mol = 1;
-//	int den = 1;
-//	int i = 0;
-//	double k = 1;
-//	double ret = 0.0;
-//	for (i = 0; i < N; i++)
-//	{
-//		ret += k * mol / den;
-//		mol++;
-//		den += 2;
-//		k *= -1;
-//	}
-//	printf("%.3lf\n", ret);
 //	return 0;
 //}
 
@@ -147,16 +20,139 @@
 //
 //int main()
 //{
-//	int m = 0;
 //	int n = 0;
-//	scanf("%d%d", &m, &n);
-//	int i = 0;
+//	scanf("%d", &n);
 //	double ret = 0.0;
-//	for (i = m; i < n + 1; i++)
+//	int i = 0;
+//	for (i = 1; i < n + 1; i++)
 //	{
-//		ret += (pow(i, 2) + 1.0 / i);
+//		ret += sqrt(i);
 //	}
 //	printf("sum = %.6lf\n", ret);
 //	return 0;
 //}
 
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = 0;
+//	int i = 0;
+//	int j = 0;
+//	for (i = 1; i <= n; i++)
+//	{
+//		int tmp = 1;
+//		for (j = 1; j <= i; j++)
+//		{
+//			tmp *= j;
+//		}
+//		ret += tmp;
+//	}
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<math.h>
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = 0;
+//	int i = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		ret += pow(2, i + 1);
+//	}
+//	printf("result = %d\n", ret);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//double Fac(int num)
+//{
+//	if (num != 1)
+//	{
+//		return 1.0*num * Fac(num - 1);
+//	}
+//	return 1.0;
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	int m = 0;
+//	double ret = 0.0;
+//	scanf("%d%d", &m, &n);
+//	if (n == m)
+//	{
+//		printf("%.0lf", 1.0);
+//	}
+//	else
+//	{
+//		ret = Fac(n) / (Fac(m)*Fac(n - m));
+//		printf("result = %.0lf", ret);
+//	}
+//	return 0;
+//}
+
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	double a = 0.0;
+	double b = 0.0;
+	double c = 0.0;
+	scanf("%lf%lf%lf", &a, &b, &c);
+	if (a == 0 && b == 0)
+	{
+		if (c == 0)
+		{
+			printf("Zero Equation");
+		}
+		else
+		{
+			printf("Not An Equation");
+		}
+	}
+	if (a == 0)
+	{
+		printf("%.2lf\n", -1 * c / b);
+	}
+	double dt = b * b - 4 * a*c;
+	if (dt)
+	{
+		if (dt > 0)
+		{
+			if (a > 0)
+			{
+				printf("%.2lf %.2lf\n", (-1 * b + sqrt(dt)) / 2 * a, (-1 * b - sqrt(dt)) / 2 * a);
+			}
+			else
+			{
+				printf("%.2lf %.2lf\n", (-b - sqrt(dt)) / 2 * a, (-b + sqrt(dt)) / 2 * a);
+			}
+		}
+		else
+		{
+			if (a > 0)
+			{
+				printf("%.2lf %.2lf\n", (-1 * b + sqrt(-1*dt)) / 2 * a, (-1 * b - sqrt(-1*dt)) / 2 * a);
+			}
+			else
+			{
+				printf("%.2lf %.2lf\n", (-b - sqrt(-1*dt)) / 2 * a, (-b + sqrt(-1*dt)) / 2 * a);
+			}
+		}
+	}
+	else
+	{
+		printf("")
+	}
+	return 0;
+}
